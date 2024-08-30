67a89ad0210ca12eb6e117777b39eb5b24c157a4
NgocAnh <158980143+Swallowg@users.noreply.github.com>
2024-08-30 17:00:48 +0700

    Fix

--- app/src/main/java/com/example/lab1randomnumber/MainActivity.java
+++ app/src/main/java/com/example/lab1randomnumber/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
         loadView();
         btn_random.setOnClickListener(view -> {
             int res = random.nextInt(6) + 1;
